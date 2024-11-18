fa65fb5b836ca00381dd115841c5b860d62e1d27
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-18 10:10:39 +0900

    state modifier

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,23 @@ export default function Home() {
       <div className="bg-white 
       w-full max-w-screen-sm 
       shadow-lg p-5 rounded-3xl 
-      flex flex-col gap-2 md:flex-row">
+      flex flex-col gap-2 md:flex-row *:outline-none
+      ring ring-transparent transition-shadow
+      has-[:invalid]:ring-red-100">
         <input className="w-full rounded-full
-         h-12 bg-gray-200 pl-5
-         outline-none 
+         h-12 bg-gray-200 pl-5 
          ring ring-transparent
          focus:ring-green-500 focus:ring-offset-2 transition-shadow
          placeholder:drop-shadow invalid:focus:ring-red-500
          peer"
-          type="email" required placeholder="email address" />
+          type="text"
+          required
+          placeholder="email address"
+        />
         <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required</span>
         <button className="bg-black to-purple-200 bg-opacity-50 text-white py-2 
         rounded-full active:scale-90 
         transition-transform font-medium
-        outline-none
         md:px-10">
           Log in
         </button>
