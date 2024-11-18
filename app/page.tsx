3ecc88310a346f507820ae1b378f273900375838
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-18 09:47:50 +0900

    responseive modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,21 @@
 export default function Home() {
   return (
-    <main className="
-    bg-gray-100
-     h-screen 
-     flex items-center justify-center
-     p-5 dark:bg-gray-700">
-      <div className="
-      bg-white 
-      w-full 
-      max-w-screen-sm 
-      shadow-lg 
-      p-5 
-      rounded-3xl dark:bg-gray-600">
-        <div className="flex justify-between">
-          <div className="flex flex-col">
-            <span className="
-            text-gray-600
-             font-semibold 
-             -mb-1 dark:text-gray-300">
-              In transit
-            </span>
-            <span className="
-            text-4xl 
-            font-semibold dark:text-white">
-              CoolBlue
-            </span>
-          </div>
-          <div className="size-12 bg-orange-400 rounded-full" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="
-          bg-green-400
-           text-white 
-           uppercase 
-           px-2.5 py-1.5 text-xs font-medium rounded-full
-           transition
-            hover:bg-green-500 hover:scale-125">
-            Today
-          </span>
-          <span className="dark:text-gray-100">9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 absolute w-full h-2 rounded-full" />
-          <div className="bg-green-400 absolute w-2/3 h-2 rounded-full" />
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white 
+      w-full max-w-screen-sm 
+      shadow-lg p-5 rounded-3xl 
+      flex flex-col gap-2 md:flex-row">
+        <input className="w-full rounded-full
+         h-12 bg-gray-200 pl-5
+         outline-none 
+         ring ring-transparent
+         focus:ring-orange-500 focus:ring-offset-2 transition-shadow
+         placeholder:drop-shadow" type="text" placeholder="search here" />
+        <button className="bg-black bg-opacity-50 text-white py-2 
+        rounded-full active:scale-90 
+        transition-transform font-medium
+        outline-none
+        md:px-10">Search</button>
       </div>
     </ main>
   )
