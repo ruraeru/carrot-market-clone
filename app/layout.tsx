f230a7de9d5ffd7ef0be1b092e51710542374c88
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-19 11:48:31 +0900

    #4.0 home screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className}
+       bg-neutral-900 text-white max-w-screen-sm
+       mx-auto`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,25 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <span>🥕</span>
-        <h1>당근</h1>
-        <h2>당근 마켓에 어서오세요!</h2>
+    <div className="flex flex-col items-center 
+    justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col
+      items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
       </div>
-      <div>
-        <Link href="/create-account">시작하기</Link>
-        <div>
+      <div className="flex flex-col items-center
+      gap-3 w-full">
+        <Link href="/create-account"
+          className="w-full bg-orange-500 text-white
+          text-lg font-medium py-2.5 rounded-md 
+          text-center hover:bg-orange-400 transition-colors">
+          시작하기
+        </Link>
+        <div className="flex gap-2">
           <span>이미 계정이 있나요?</span>
-          <Link href="/login">로그인</Link>
+          <Link href="/login" className="hover:underline underline-offset-2">로그인</Link>
         </div>
       </div>
     </div>
