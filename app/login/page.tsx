b58c8b322ba4ed92af6dc485923bddfcca3cc7a3
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-20 13:19:49 +0900

    api router and route handlers

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import FormButton from "@/components/form-button";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function Login() {
+    const onClick = async () => {
+        const response = await fetch("/api/users", {
+            method: "POST",
+            body: JSON.stringify({
+                username: "nico",
+                password: "1234"
+            })
+        })
+        console.log(await response.json());
+    }
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 
@@ -24,11 +36,10 @@ export default function Login() {
                     errors={[]}
                     required
                 />
-                <FormButton
-                    loading={false}
-                    text="Create account"
-                />
             </form>
+            <span onClick={onClick}>
+                <FormButton loading={false} text="Log in" />
+            </span>
             <SocialLogin />
         </div>
     )
