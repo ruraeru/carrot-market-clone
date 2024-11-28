1b140333b2e17f051bf40b75376d3a27fa4cefab
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-28 23:32:55 +0900

    edge function

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,10 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
-export async function middleware(req: NextRequest) {
-  console.log("Hello");
-}
+//middleware === edge funtion
+//빠르게 실행되야하고 경량화 NodeJS API라 지원하지 않는 패키지들이 많음
+//https://nextjs.org/docs/pages/api-reference/edge
+export async function middleware(req: NextRequest) {}
 
 //middleware가 실행 될 곳을 지정 가능
 export const config = {
