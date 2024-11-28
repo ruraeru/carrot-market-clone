0ab97fc9b28bfb53c99df9828b5ea7b7e3d349ba
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-28 23:23:00 +0900

    matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(req: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (req.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", req.url));
-  }
+  console.log("Hello");
 }
+
+//middleware가 실행 될 곳을 지정 가능
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
