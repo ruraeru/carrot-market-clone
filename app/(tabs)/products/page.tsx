c8cec3f658ff74deb2908249d0c1addefbf346da
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-12-03 11:39:34 +0900

    Skeletons, loading components

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,4 +1,9 @@
-export default function Product() {
+async function getProducts() {
+    await new Promise(resolve => setTimeout(resolve, 10000))
+}
+
+export default async function Product() {
+    const products = await getProducts()
     return (
         <div>
             <h1 className="text-white text-4xl">Product!</h1>
