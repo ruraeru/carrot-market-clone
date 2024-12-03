694b7f0751ce99c9031c0561bed74b2ff847ad0b
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-12-03 12:09:25 +0900

    Product Compoentns

---
+++ components/list-product.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ListProductProps {
+    id: number;
+    title: string;
+    price: number;
+    created_at: Date;
+    photo: string;
+}
+export default function ListProduct({ id, title, price, created_at, photo }: ListProductProps) {
+    return (
+        <Link href={`/products/${id}`} className="flex gap-5">
+            <div className="relative size-28 rounded-md overflow-hidden">
+                <Image fill src={photo} alt={title} />
+            </div>
+            <div className="flex flex-col gap-1 *:text-white">
+                <span className="text-lg">{title}</span>
+                <span className="text-sm text-neutral-500">{created_at.toString()}</span>
+                <span className="text-lg font-semibold">{price}</span>
+            </div>
+        </Link>
+    )
+}
