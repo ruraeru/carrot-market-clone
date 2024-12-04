46455608ccab9e260a40825b31b74fba62b57421
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-12-04 11:58:10 +0900

    Product Upload

---
+++ app/products/add/actions.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import fs from "fs/promises";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "Photo is required",
+  }),
+  title: z.string({
+    required_error: "Title is required",
+  }),
+  description: z.string({
+    required_error: "Description is required",
+  }),
+  price: z.coerce.number(),
+});
+
+export async function uploadProduct(_: any, formData: FormData) {
+  const data = {
+    title: formData.get("title"),
+    photo: formData.get("photo"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+  };
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+  const results = productSchema.safeParse(data);
+  if (!results.success) {
+    return results.error.flatten();
+  } else {
+    const { title, description, photo, price } = results.data;
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title,
+          photo,
+          price,
+          description,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
+}
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -3,30 +3,53 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import { useActionState, useState } from "react";
+import { uploadProduct } from "./actions";
 
 export default function AddProduct() {
     const [preview, setPreview] = useState("");
-    const onImageChange = () => {
+    const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { target: { files } } = e;
+        if (!files) return;
+        const file = files[0];
 
+        const allowedFileTypes = ["png", "jpg", "jpeg"];
+        if (allowedFileTypes.indexOf(file.type.split("/")[1]) === -1) {
+            alert("file upload is only .png, .jpg, .jpeg");
+            return;
+        }
+        if (file.size > 4000000) {
+            alert("file is very big!!!!");
+            return;
+        }
+        setPreview(URL.createObjectURL(file));
     }
+    const [state, action] = useActionState(uploadProduct, null);
     return (
         <div>
-            <form className="flex flex-col gap-5 p-5">
+            <form action={action} className="flex flex-col gap-5 p-5">
                 <label
                     htmlFor="photo"
                     className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed
-                    cursor-pointer"
+                    cursor-pointer bg-center bg-no-repeat bg-cover"
+                    style={{
+                        backgroundImage: `url(${preview})`
+                    }}
                 >
-                    <PhotoIcon className="w-20" />
-                    <div className="text-neutral-400 text-sm">
-                        사진을 추가해주세요.
-                    </div>
+                    {!preview ? (
+                        <>
+                            <PhotoIcon className="w-20" />
+                            <div className="text-neutral-400 text-sm">
+                                사진을 추가해주세요.
+                                {state?.fieldErrors.photo}
+                            </div>
+                        </>
+                    ) : null}
                 </label>
-                <input onChange={onImageChange} type="file" id="photo" name="photo" className="hidden" />
-                <Input name="title" required placeholder="제목" type="text" />
-                <Input name="price" required placeholder="가격" type="number" />
-                <Input name="description" type="text" required placeholder="자세한 설명" />
+                <input onChange={onImageChange} type="file" id="photo" name="photo" accept="image/*" className="hidden" />
+                <Input name="title" required placeholder="제목" type="text" errors={state?.fieldErrors.title} />
+                <Input name="price" required placeholder="가격" type="number" errors={state?.fieldErrors.price} />
+                <Input name="description" type="text" required placeholder="자세한 설명" errors={state?.fieldErrors.description} />
                 <Button text="작성 완료" />
             </form>
         </div>
