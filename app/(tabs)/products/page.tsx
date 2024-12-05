8e624f25847fc0f0e5e238a23cc095c5ab4d4f56
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-12-05 10:34:45 +0900

    revalidatePath

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -2,11 +2,11 @@ import ProductList from "@/components/product-list";
 import db from "@/lib/db"
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
-import { unstable_cache as nextCache } from "next/cache";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 import Link from "next/link";
 
 const getCachedProducts = nextCache(getInitialProducts, ["home-products"], {
-    revalidate: 60
+    revalidate: 60,
 })
 
 async function getInitialProducts() {
@@ -35,9 +35,16 @@ export const metadata = {
 
 export default async function Products() {
     const initialProducts = await getCachedProducts()
+    const revalidate = async () => {
+        "use server"
+        revalidatePath("/products")
+    }
     return (
         <div>
             <ProductList initialProducts={initialProducts} />
+            <form action={revalidate}>
+                <button>Revalidate</button>
+            </form>
             <Link href="/products/add" className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400">
                 <PlusIcon className="size-10" />
             </Link>
