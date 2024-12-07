b8db37bc8582f17d88c4b42c881cdeeb05ae8faa
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-12-07 11:12:07 +0900

    Likes and dislikes

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,4 +1,15 @@
-export default function Page() {
+import { notFound } from "next/navigation";
+
+async function getPost(id: number) {
+
+}
+
+export default async function PostDetail({ params }: { params: { id: string } }) {
+    const { id } = await params;
+    const paramsId = Number(id);
+    if (isNaN(paramsId)) {
+        return notFound();
+    }
     return (
         <div>
             asdas
